8ce3d13981e2f405fa1d1f25a3c885691bc7574e
Tal Liron <tal.liron@gmail.com>
2025-08-12 09:54:20 -0500

    Combine crates into one

---
+++ cli-macros/src/derive_depict/enum_generator/impl_depict.rs
@@ -0,0 +1,47 @@
+use super::generator::*;
+
+use {proc_macro2::*, quote::*};
+
+impl EnumGenerator {
+    /// Generate `impl Depict`.
+    pub fn generate_impl_depict(&self) -> TokenStream {
+        let mut segments = Vec::default();
+
+        for variant in &self.variants {
+            segments.push(self.generate_handle_variant(variant));
+        }
+
+        let enum_name = &self.enum_name;
+        let (impl_generics, struct_generics, where_clause) = self.enum_generics.split_for_impl();
+
+        quote! {
+            #[automatically_derived]
+            impl
+                #impl_generics
+                ::kutil::cli::depict::Depict
+                for #enum_name #struct_generics
+                #where_clause
+            {
+                fn
+                    depict
+                    <WriteT>
+                    (
+                        &self,
+                        writer: &mut WriteT,
+                        context: &::kutil::cli::depict::DepictionContext,
+                    )
+                    -> ::std::io::Result<()>
+                    where WriteT: ::std::io::Write
+                {
+                    let context = &context.child().with_separator(true);
+
+                    match self {
+                        #(#segments)*
+                    }
+
+                    Ok(())
+                }
+            }
+        }
+    }
+}
